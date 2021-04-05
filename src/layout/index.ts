1ecb16f2fc5f279ebb10dca5687fe0e4eae65ed4
Will Dady <will@good.work>
2021-04-05 13:13:31 +1000

    refactor: removed old block types

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,63 +1,9 @@
-type Section<C extends string[]> = {
-  background?: boolean;
-  vAlign?: 'top' | 'center' | 'bottom';
-  components: C;
-  flexGrow?: number;
-  maxWidth?: number;
-};
-
-export type OneColumnBlock<S1 extends string[] = string[]> = {
-  type: 'one-column';
-  sections: {
-    one: Section<S1>;
-  };
-};
-
-export type TwoColumnBlock<
-  S1 extends string[] = string[],
-  S2 extends string[] = string[]
-> = {
-  type: 'two-column';
-  sections: {
-    one: Section<S1>;
-    two: Section<S2>;
-  };
-};
-
-export type ThreeColumnBlock<
-  S1 extends string[] = string[],
-  S2 extends string[] = string[],
-  S3 extends string[] = string[]
-> = {
-  type: 'three-column';
-  sections: {
-    one: Section<S1>;
-    two: Section<S2>;
-    three: Section<S3>;
-  };
-};
-
-export type FourColumnBlock<
-  S1 extends string[] = string[],
-  S2 extends string[] = string[],
-  S3 extends string[] = string[],
-  S4 extends string[] = string[]
-> = {
-  type: 'four-column';
-  sections: {
-    one: Section<S1>;
-    two: Section<S2>;
-    three: Section<S3>;
-    four: Section<S4>;
-  };
-};
-
-export type RowBlock = {
+export type RowBlock<T extends string = string> = {
   type: 'row';
   props: {
     columns: {
       components: {
-        id: string;
+        id: T;
         align?: 'left' | 'center' | 'right';
       }[];
       background?: boolean;
@@ -67,9 +13,4 @@ export type RowBlock = {
   };
 };
 
-export type Block =
-  | OneColumnBlock
-  | TwoColumnBlock
-  | ThreeColumnBlock
-  | FourColumnBlock
-  | RowBlock;
+export type Block = RowBlock;
